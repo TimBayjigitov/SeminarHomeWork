c5aa1b214cb8a205bdbe1cdb1ef5ca6b20f4d949
Tim <phantomxp797@gmail.com>
2022-06-23 13:51:10 +0500

    add homework

---
+++ HomeWork_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее:
+int n1; 
+int n2;
+Console.WriteLine("Введите числа: ");
+n1 = Convert.ToInt32(Console.ReadLine());
+n2 = Convert.ToInt32(Console.ReadLine());
+if(n1 > n2)
+{
+    Console.WriteLine($"{n1} max, {n2} min");
+}
+else
+    if(n1 < n2)
+    {
+        Console.WriteLine($"{n2} max, {n1} min");
+    }
+    else
+        if(n1 == n2)
+        {
+            Console.WriteLine($"{n1} = {n2}");
+        }
---
+++ HomeWork_4/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+int N;
+int Count = 1;
+Console.WriteLine("Введите число: ");
+N = Convert.ToInt32(Console.ReadLine());
+while (Count <= N)//Предполагаем, что в условии задания N - включая.
+{
+    if(Count%2 == 0)
+    {
+           Console.Write(Count + " "); 
+    }
+Count++;
+}
